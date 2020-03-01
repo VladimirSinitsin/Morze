eeb254502b53a97288f1c783db8a0f8607c4ea16
Vladimir Sinitsin <vladimir.sinitsin.ya@yandex.ru>
2020-03-01 18:49:26 +0300

    Coder; Fix Front; Download Beep

--- Morze.py
+++ Morze.py
@@ -1,6 +1,8 @@
 from tkinter import *
 import time
 
+from coder import morze
+
 window = Tk()
 window.title("Morze")
 window.geometry('1000x600+450+250')
@@ -10,14 +12,19 @@ message = StringVar()
 lbl = Label(window, text="HELL0 MY FRIEND", font=("Fira Mono OT", 50))
 lbl.place(x=200, y=50)
 
+runl = Label(window, font=("Fira Mono OT", 20))
+runl.place(x=300, y=200)
+runl.focus()
+
 txt = Entry(window, textvariable=message, font=("Fira Mono OT", 10), width=75)
-txt.place(x=200, y=300)
+txt.place(x=200, y=300)			
 
 def show():
-	text = ' ' * 10 + message.get()
+	text = ' ' * 25 + morze(message.get())
 	for i in range(len(text)):
-		time.sleep(0.3)
-		lbl.config(text=text[i:i+10])
+		time.sleep(0.4)
+		runl.config(text=text[i:i+25])
+		runl.update()
 		
 def show_message():
     lbl.config(text=message.get())
