1b9f85a7ad0c62b1d56ea8315def0604963fa3cb
Vladimir Sinitsin <vladimir.sinitsin.ya@yandex.ru>
2020-03-01 16:49:59 +0300

    Фронт без БС (жду помощи разработчика)

--- Morze.py
+++ Morze.py
@@ -1,13 +1,28 @@
 from tkinter import *
+import time
 
 window = Tk()
 window.title("Morze")
 window.geometry('1000x600+450+250')
 
+message = StringVar()
+
 lbl = Label(window, text="HELL0 MY FRIEND", font=("Fira Mono OT", 50))
 lbl.place(x=200, y=50)
 
-txt = Entry(window,font=("Fira Mono OT", 10), width=10)
-txt.place(x=200, y=150)
+txt = Entry(window, textvariable=message, font=("Fira Mono OT", 10), width=75)
+txt.place(x=200, y=300)
+
+def show():
+	text = ' ' * 10 + message.get()
+	for i in range(len(text)):
+		time.sleep(0.3)
+		lbl.config(text=text[i:i+10])
+		
+def show_message():
+    lbl.config(text=message.get())
+
+btn = Button(text="Text.ToMorze()", command=show)
+btn.place(x=450, y=350)
 
 window.mainloop()
